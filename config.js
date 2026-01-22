4183c0bfdc221e3497af4a9d021171eeab45c0f4
Purusotam-99 <167285264+Purusotam-99@users.noreply.github.com>
2026-01-22 20:52:22 +0530

    Switch to GNews API to fix CORS issue

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Your NewsAPI key
-const API_KEY = "1124ebd2a99445e7a9855e5c53d44f90";
+const API_KEY = "22f3a2c36e2e1e89640545d2345ead8d";
+const BASE_URL = "https://gnews.io/api/v4/top-headlines?lang=en&country=us";
 
-// Base URL for top headlines
-const BASE_URL = "https://newsapi.org/v2/top-headlines?country=us";
+
